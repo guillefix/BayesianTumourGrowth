3e6bcc64c481a14d76f9c6a92ec0edc214226bce
Maxandre Jacqueline <maxandre.jacqueline@me.com>
2017-03-20 16:28:19 +0000

    added mean and standard deviation to excel spreadsheet, modification of ABC to fit real data

--- ode_model.m
+++ ode_model.m
@@ -2,7 +2,7 @@ function dVdt = ode_model(t, V, rc, Kc,lc,rr,Kr,lr)
 
 dVdt = zeros(2,1);
 
-dVdt(1) = V(1)*(rc*(1-V(1)/Kc)-lr*V(2));
-dVdt(2) = V(2)*(rr*(1-V(2)/Kr)-lc*V(1));
+dVdt(1) = V(1)*(rc*(1-V(1)/Kc)-lr*V(2)); % V_c
+dVdt(2) = V(2)*(rr*(1-V(2)/Kr)-lc*V(1)); % V_r
 
 
